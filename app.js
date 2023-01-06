540349ba6449f8055db91add1c0234f9eaaa556d
Gerard Solanes <gerardsolher@gmail.com>
2023-01-06 20:21:27 +0100

    Set up spotify API

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const SpotifyWebApi = require('spotify-web-api-node');
 
 const express = require('express');
 const hbs = require('hbs');
@@ -12,6 +13,16 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+  });
+  
+  // Retrieve an access token
+  spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
 // Our routes go here:
 
