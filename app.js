7b74c9cc8fd17180df1ce43eb606b14846dde749
Gerard Solanes <gerardsolher@gmail.com>
2023-01-07 17:45:36 +0100

    Request API for artist in  GET /artist-search

--- app.js
+++ app.js
@@ -27,6 +27,17 @@ const spotifyApi = new SpotifyWebApi({
 // Our routes go here:
 app.get('/', (req, res, next) => {
     res.render('index');
+});
+
+app.get('/artist-search', (req, res, next) => {
+  const { artist } = req.query;
+  spotifyApi
+    .searchArtists(artist)
+    .then(data => {
+      console.log('The received data from the API: ', data.body);
+      // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+    })
+    .catch(err => console.log('The error while searching artists occurred: ', err));
 })
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
